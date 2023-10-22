bc5404e9b114de4a8e9b3845d2ecdb73681fa832
nikamaish <100349291+nikamaish@users.noreply.github.com>
2023-10-22 13:25:44 +0530

    Add files via upload
    src folder is added

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+// import CryptoGraphs from './Cryptographs'
+import Services from './services/Services'
+// import cryptoData from './cryptoData/Cryptodata'
+import './broadcast/Broadcast' 
+import Broadcast from './broadcast/Broadcast'
+// import RealTimeData from './cryptoData/Cryptodata'
+// import CryptoWidget from './cryptoData/CryptoWidget'
+
+const App = () => {
+  return (
+    <div className='App'>
+  {/* <CryptoGraphs/>       */}
+  <Services/>
+  {/* <CryptoWidget/> */}
+  {/* <RealTimeData/> */}
+  {/* <Broadcast/> */}
+  
+    </div>
+  )
+}
+
+export default App
