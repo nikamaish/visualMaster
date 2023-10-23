0a4502627d33db4b338a19b8cf1b4e0678cf0743
nikamaish <100349291+nikamaish@users.noreply.github.com>
2023-10-23 19:54:43 +0530

    Add files via upload
    new is added which are shown in review2

--- src/App.js
+++ src/App.js
@@ -4,17 +4,35 @@ import Services from './services/Services'
 // import cryptoData from './cryptoData/Cryptodata'
 import './broadcast/Broadcast' 
 import Broadcast from './broadcast/Broadcast'
+import Headsection from './headSection/Headsection'
+import Navbar from './navbar/Navbar'
 // import RealTimeData from './cryptoData/Cryptodata'
 // import CryptoWidget from './cryptoData/CryptoWidget'
+import Footer from './Footer/Footer'
+import RiskWarning from './Risk/Risk'
+import Help from './help/Help'
+import CryptoData from './cryptoData/CryptoData'
 
 const App = () => {
   return (
     <div className='App'>
   {/* <CryptoGraphs/>       */}
-  <Services/>
+  {/* <Services/> */}
   {/* <CryptoWidget/> */}
   {/* <RealTimeData/> */}
-  {/* <Broadcast/> */}
+  
+  
+  <Navbar/>
+  <Broadcast/>
+  <Headsection/>
+  <CryptoData/>
+  <Services/>
+  <Help/>
+  <RiskWarning/>
+  <Footer/>
+
+
+
   
     </div>
   )
